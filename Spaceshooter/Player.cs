5d5e9f142cf4d683705a6ecbabde065905292859
MR_5 <43605555+Diablobounds@users.noreply.github.com>
2019-10-17 12:22:08 +0200

    Håller på med player-klassen

---
+++ Spaceshooter/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Spaceshooter
+{
+    class Player
+    {
+
+        Texture2D ship_texture;
+        Vector2 ship_vector;
+        Vector2 ship_speed;
+
+    }
+}
